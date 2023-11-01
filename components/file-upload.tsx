4acffebd5336782aaeb7cbc415c5fe91e28edbef
Nothingg <nothing9537@gmail.com>
2023-11-01 17:10:03 -0500

    Addd image view into FileUpload component

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { FC } from 'react';
+import { X } from 'lucide-react';
+import Image from 'next/image';
 
 import { UploadDropzone } from '@/lib/uploadthing';
 import { OurFileRouter } from '@/app/api/uploadthing/core';
@@ -10,10 +12,27 @@ import "@uploadthing/react/styles.css";
 interface FileUploadProps {
   endpoint: keyof OurFileRouter;
   onChange: (url?: string) => void;
-  value: string;
+  value?: string;
 }
 
 export const FileUpload: FC<FileUploadProps> = ({ endpoint, onChange, value }) => {
+  const fileType = value?.split('.').pop();
+
+  if (value && fileType !== 'pdf') {
+    return (
+      <div className='relative h-20 w-20'>
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          className='bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm'
+          type='button'
+          onClick={() => onChange('')}
+        >
+          <X className='h-4 w-4' />
+        </button>
+      </div>
+    )
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
