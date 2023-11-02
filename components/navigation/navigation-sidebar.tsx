9e48cafbbb5669b431ca42be23ad0e7eca327ae9
Nothingg <nothing9537@gmail.com>
2023-11-02 10:32:04 -0500

    add ThemeChange switcher, logout button

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,13 +1,15 @@
-import { FC, useCallback } from 'react';
+import { FC } from 'react';
 import { redirect } from 'next/navigation';
+import { UserButton } from '@clerk/nextjs';
 
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 
-import { NavigationAction } from './navigation-action';
 import { Separator } from '../ui/separator';
 import { ScrollArea } from '../ui/scroll-area';
+import { NavigationAction } from './navigation-action';
 import { NavigationItem } from './navigation-item';
+import { ModeToggle } from '../mode-toggle';
 
 export const NavigationSidebar: FC = async () => {
   const profile = await currentProfile();
@@ -27,7 +29,7 @@ export const NavigationSidebar: FC = async () => {
   });
 
   return (
-    <div className='space-y-4 flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3'>
+    <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3'>
       <NavigationAction />
       <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto' />
       <ScrollArea className='flex-1 w-full'>
@@ -37,6 +39,17 @@ export const NavigationSidebar: FC = async () => {
           </div>
         ))}
       </ScrollArea>
+      <div className='pb-3 mt-auto flex items-center flex-col gap-y-4'>
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl='/'
+          appearance={{
+            elements: {
+              avatarBox: 'h-[48px] w-[48px]'
+            }
+          }}
+        />
+      </div>
     </div>
   );
 };
