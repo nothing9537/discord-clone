941db6171350fd006f63cd99291df59d092e31cf
Nothingg <nothing9537@gmail.com>
2023-11-03 18:07:57 -0500

    Update navigation-sidebar.tsx

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -29,7 +29,7 @@ export const NavigationSidebar: FC = async () => {
   });
 
   return (
-    <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3'>
+    <div className='space-y-4 flex flex-col items-center h-full text-primary w-full bg-zinc-200 dark:bg-[#1e1f22] py-3'>
       <NavigationAction />
       <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto' />
       <ScrollArea className='flex-1 w-full'>
