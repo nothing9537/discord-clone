aa233f852a179e737b5e6f9e3866db97bf2d3124
Nothingg <nothing9537@gmail.com>
2024-08-22 13:17:17 -0500

    test fx

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db = globalThis?.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = db;
